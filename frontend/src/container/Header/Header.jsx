bb72d1851e8d540d0f848389832bc053d6807c68
Luis Carlos <lucaldev22@gmail.com>
2022-04-05 18:54:35 -0300

    Nav bar and assets

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {motion} from 'framer-motion';
+import {images} from '../../constants';
 
 import './Header.scss'
 
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='app__header app__flex'>
+      <motion.div
+      whileInView={{x:[-100,0],opacity:[0,1]}}
+      transition={{duration: 0.5}}
+      className="app__header-info"
+      >
+        
+      </motion.div>
+
+    </div>
   )
 }
 
