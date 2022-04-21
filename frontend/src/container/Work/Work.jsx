8614586a972d6639b2340b95f7d4e7630515f72c
Luis Carlos <lucaldev22@gmail.com>
2022-04-21 20:17:00 -0300

    Work section almost complete, remove some imgs.

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from './props/WorkCard'
+import images from '../../constants/images'
 
 
 import './Work.scss'
@@ -8,7 +9,40 @@ const Work = () => {
   return (
     <section className='app__work'>
       <div className='app__work-content'>
-        <Card />
+        <div className='app__work-title'>        
+          <h1>Projetos</h1>
+          <p>Esses são alguns dos projetos que desenvolvi.</p>
+        </div>
+        <div className='app__work-portfolio'>
+          <Card
+            title='Portfolio'
+            link='portfolio.com.br'
+            img={[images.html, images.css, images.react]}
+            background={images.portfolio}
+            githubLink='#'
+          />
+          <Card
+            title='Keeper App'
+            link='KeeperApp.com.br'
+            img={[images.html, images.css, images.react]}
+            background={images.keeper}
+            githubLink='https://github.com/Lucal22/Keeper-app'
+          />
+          <Card
+            title='Keeper App'
+            link='KeeperApp.com.br'
+            img={[images.html, images.css, images.react]}
+            background={images.keeper}
+            githubLink='https://github.com/Lucal22/Keeper-app'
+          />
+          <Card
+            title='Keeper App'
+            link='KeeperApp.com.br'
+            img={[images.html, images.css, images.react]}
+            background={images.keeper}
+            githubLink='https://github.com/Lucal22/Keeper-app'
+          />
+        </div>
       </div>
     </section>
   )
--- frontend/src/container/Work/props/WorkCard.jsx
+++ frontend/src/container/Work/props/WorkCard.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import images from '../../../constants/images';
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='app__work-card'>
-            <div className="app__work-card-content">
+            <div className='app__work-card-flex'>
+            <div className="app__work-card-content"
+                style={
+                    { backgroundImage: `url(${props.background}) ,linear-gradient(1deg, rgba(0, 0, 0, 0.33) 30%, rgba(255, 255, 255, 0.25) 100%)` }}>
                 <div className="app__work-card-skills">
-                    <h1>Content</h1>
+                    <div>
+                        {props.img.map((icons) => (<img src={icons} alt="Símbologia da linguagem utilizada" />))}
+                    </div>
+                    <div>
+                        <a href={props.githubLink}><img src={images.github} alt="Símbolo github" /></a>
+                    </div>
                 </div>
             </div>
-            <div className="app__work-card-title">
-            <h1>Keeper App</h1>
-            <img src={images.arrow} alt="Ver detalhes"></img>
+            <div className="app__work-card-description">
+                <h1>{props.title}</h1>
+                <p><a href={props.link}>{props.link}</a></p>
+            </div>
             </div>
         </div>
     )
